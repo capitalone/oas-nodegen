a923f397c3fd19cf2d896a89b36600ba31ad1288
Phil Kedy <phil.kedy@gmail.com>
2018-01-22 10:46:25 -0500

    Added SPDX header lines per issue #1

--- test/modules/helpers.js
+++ test/modules/helpers.js
@@ -1,4 +1,6 @@
 /*
+ * SPDX-Copyright: Copyright (c) Capital One Services, LLC
+ * SPDX-License-Identifier: Apache-2.0
  * Copyright 2016 Capital One Services, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
